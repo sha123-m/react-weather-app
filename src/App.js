263892026e23e3a662a3d2e332901e58e29d0f65
Sharon Mwenda <79165122+sha123-m@users.noreply.github.com>
2025-04-16 12:42:43 +0300

    added github repo url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by sha123-m and is {""}
+    <a href="https://github.com/sha123-m/react-weather-app" target="_blank" rel="noreferrer">
+     open sourced on github
+    </a>
+    </footer>
+    </div>
   );
 }
 
