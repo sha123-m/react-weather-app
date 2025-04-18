3e0be8b1359be725505ec3476ae5a1f184f3e8d2
Sharon Mwenda <79165122+sha123-m@users.noreply.github.com>
2025-04-18 18:32:07 +0300

    added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,41 +1,73 @@
-import React from "react";
-import"./Weather.css";
+import React, { useState } from "react";
+import axios from "axios";
+import "./Weather.css";
 
-export default function weather(){
-    return <div className="Weather">
+export default function Weather() {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    console.log(response.data);
+
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      date: "Wednesday 07:00", // You might want to make this dynamic later
+      description: response.data.condition.description,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
         <form>
-            <div className="row">
-                <div className="col-9">
-            <input type="text" placeholder="Enter a city" className="form-control" autoFocus="on"/>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="text"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
+              />
             </div>
             <div className="col-3">
-            <input type ="button" value="search" className="btn btn-primary w-100"/>
+              <input
+                type="button"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
-            </div>
+          </div>
         </form>
-        <h1>New York</h1>
+        <h1>{weatherData.city}</h1>
         <ul>
-            <li>wednesday 7:00</li>
-            <li>Mostly cloudy</li>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
         </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-           
-            <img src=
-            "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="mostly cloudy" />
-            
-           <span className="temperature">6</span>
-           <span className="unit">°C</span>
-           </div>
-       
-        <div className="col-6">
+        <div className="row mt-3">
+          <div className="col-6">
+            <img src={weatherData.iconUrl} alt={weatherData.description} />
+            <span className="temperature">{Math.round(weatherData.temperature)}</span>
+            <span className="unit">°C</span>
+          </div>
+          <div className="col-6">
             <ul>
-                <li>precipitation:15%</li>
-                <li>humidity:72%</li>
-                <li>wind:15km/hr</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/hr</li>
             </ul>
+          </div>
         </div>
-
       </div>
-        </div>;
+    );
+  } else {
+    const apiKey ="30f662094dfb505f3tcofbd6404ca0a7";
+    let city = "New York";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
